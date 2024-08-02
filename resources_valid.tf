e89f4af5ac0926350f5a2b591a3be737ee3771b5
root <root@bti-nw69x2h6qpqugquexpc4r.us-west4-a.c.boreal-rain-289205.internal>
2024-08-02 00:46:02 +0000

    Updated  S3 bucket value in provider.tf file

--- resources_valid.tf
+++ resources_valid.tf
@@ -1,29 +1,29 @@
-# resource "aws_subnet" "public-subnet" {
-#   cidr_block        = var.public_subnet_cidr
-#   vpc_id            = aws_vpc.stack-example-vpc.id
-#   availability_zone = "us-west-2a"
+resource "aws_subnet" "public-subnet" {
+  cidr_block        = var.public_subnet_cidr
+  vpc_id            = aws_vpc.stack-example-vpc.id
+  availability_zone = "us-west-2a"
 
-#   tags = {
-#     department = "Learning"
-#     owner      = "ASE"
-#   }
-# }
+  tags = {
+    department = "Learning"
+    owner      = "ASE"
+  }
+}
 
-# resource "aws_internet_gateway" "gateway" {
-#   vpc_id = aws_vpc.stack-example-vpc.id
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.stack-example-vpc.id
 
-#   tags = {
-#     department = "Learning"
-#     owner      = "ASE"
-#   }
-# }
+  tags = {
+    department = "Learning"
+    owner      = "ASE"
+  }
+}
 
-# resource "aws_vpc" "stack-example-vpc" {
-#   cidr_block           = var.vpc_cidr
-#   enable_dns_support   = true
-#   enable_dns_hostnames = true
-#   tags = {
-#     department = "Learning"
-#     owner      = "ASE"
-#   }
-# }
+resource "aws_vpc" "stack-example-vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    department = "Learning"
+    owner      = "ASE"
+  }
+}
